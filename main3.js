3abb0cb26a0f9f3df713d362b3dfb7d711ee196a
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2016-11-04 21:08:14 +0100

    a few improvements

--- main3.js
+++ main3.js
@@ -1,45 +1,44 @@
 $(function() {
-	
-	BodyMassIndex(58,1.64);
-});	
+    BodyMassIndex(58, 1.64);
+});
 
 function BodyMassIndex(m, h) {
+    var BMI;
 
-var BMI;
+    if (!(isNumber(m) && isNumber(h))) {
+        console.log("Все параметры должны быть числами");
+        return;
+    }
 
-if (!(isNumber(m) && isNumber(h))) {
-	console.log("Все параметры должны быть числами");
-	return;
-}	
+    if (h > 3) {
+        console.log("Рост должен быть указан в метрах");
+        return;
+    }
 
-if (h>3) {
-	console.log("Рост должен быть указан в метрах");
-	return;
-}	
+    BMI = m / (h * h);
 
-BMI = m / (h * h);
+    if( BMI <= 5 || BMI > 250) {
+        console.log("Введены неверные параметры");
+        return;
+    }
 
-if (BMI > 5 && BMI <= 16) {
-	console.log("Индекс массы тела = "  + BMI);
-	console.log("Выраженный дефицит массы тела");
-} else if (BMI > 16 && BMI <= 18.5) {
-	console.log("Индекс массы тела = "  + BMI);
-	console.log("Недостаточная (дефицит) масса тела");
-} else if (BMI > 18.5 && BMI <= 25) {
-	console.log("Индекс массы тела = "  + BMI);
-	console.log("Норма");	
-} else if (BMI > 25 && BMI <= 30) {
-	console.log("Индекс массы тела = "  + BMI);
-	console.log("Избыточная масса тела (предожирение)");
-} else if(BMI > 30 && BMI <= 250) {
-	console.log("Индекс массы тела = "  + BMI);
-	console.log("Ожирение");
-}	else {
-	console.log("Введены неверные параметры");
-}
+    BMI = Math.round(BMI * 100) / 100;
 
+    console.log("Индекс массы тела = "  + BMI);
+
+    if (BMI > 30) {
+        console.log("Ожирение");
+    } else if (BMI > 25) {
+        console.log("Избыточная масса тела (предожирение)");
+    } else if (BMI > 18.5) {
+        console.log("Норма");
+    } else if (BMI > 16) {
+        console.log("Недостаточная (дефицит) масса тела");
+    } else {
+        console.log("Выраженный дефицит массы тела");
+    }
 }
 
 function isNumber(a) {
-	return typeof a === "number";
+    return typeof a === "number";
 }
