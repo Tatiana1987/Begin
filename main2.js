352b7e2825443414dfe4bb32be2623341eb284f7
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2016-11-04 20:46:58 +0100

    a few improvements

--- main2.js
+++ main2.js
@@ -1,33 +1,42 @@
 $(function() {
-	
-	triangleSquare(7, 8, 10);
-});	
+    triangleSquare(7, 8, 10);
+});
 
 function triangleSquare(a, b, c) {
+    var p, s;
 
-var p, s;
+    if (!(isNumber(a) && isNumber(b) && isNumber(c))) {
+        console.log("Все параметры должны быть числами");
+        return;
+    }
 
-if (!(isNumber(a) && isNumber(b) && isNumber(c))) {
-	console.log("Все параметры должны быть числами");
-	return;
-}
-
-if (a <= 0 || b <= 0 || c <= 0) {
-	console.log("Все параметры должны быть положительными");
-	return;
-}  
+    if (a <= 0 || b <= 0 || c <= 0) {
+        console.log("Все параметры должны быть положительными");
+        return;
+    }
 
 
-if ((a + b) < c || (a + c) < b || (b + c) < a) {
-	console.log("Треугольник не существует");
-	return;	
-}
+    if (isCorrectTriangle(a, b, c)) {
+        console.log("Треугольник не существует");
+        return;
+    }
 
-p = (a + b + c) / 2;
-s = Math.sqrt(p*(p - a)*(p - b)*(p - c)); 
-console.log("Площадь треугольника = " + s);
+    p = (a + b + c) / 2;
+    s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    console.log("Площадь треугольника = " + s);
 }
 
 function isNumber(a) {
-	return typeof a === "number";
+    return typeof a === "number";
 }
+
+function isCorrectTriangle(a, b, c) {
+    isCorrectEdge(a, b, c) ||
+    isCorrectEdge(a, c, b) ||
+    isCorrectEdge(b, c, a))
+}
+
+function isCorrectEdge(a, b, c) {
+    return (a + b) < c;
+}
+
